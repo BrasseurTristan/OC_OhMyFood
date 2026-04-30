fc21c53fb1142566d6f2a64baee73c5f1ebe5815
BrasseurTristan <brasseur.tristan@gmail.com>
2026-04-30 13:27:29 +0200

    create page restaurant and not-found page

--- src/app/restaurant/[slug]/page.jsx
+++ src/app/restaurant/[slug]/page.jsx
@@ -1,20 +1,48 @@
 import styles from "./page.module.css";
 import { restaurants } from "@/data/restaurants.json";
+import Image from 'next/image'
+import FavoriteButton from "@/components/FavoriteButton/FavoriteButton";
+import MenuItem from "@/app/restaurant/[slug]/_component/MenuItem/MenuItem";
+import { notFound } from "next/navigation";
 
 export default async function RestaurantDetail({ params }){
     const { slug } = await params;
     const restaurant = restaurants.find((restaurant) => restaurant.slug === slug);
-
+    
     if(!restaurant){
-        return(
-             <div className={styles.container}>
-                <h1>Restaurant non trouvée</h1>
-                <p>Ce restaurant n&apos;existe pas ou a été supprimée.</p>
-            </div>
-        );
+        return notFound();
     }
 
     return(
-        
+            <>
+            <div className={styles.heroImage}>
+                <Image src={restaurant.image} alt={restaurant.name}  fill priority className={styles.image}/>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.restaurantHeader}>
+                    <h2>{restaurant.name}</h2>
+                    <FavoriteButton/>
+                </div>
+                <div className={styles.menuContainer} >
+                    <h3>ENTRÉES</h3>
+                    {restaurant.menu.entrées.map((m) => (
+                        <MenuItem  item={m}/>
+                    ))}
+                </div>
+                <div className={styles.menuContainer}>
+                    <h3>PLATS</h3>
+                    {restaurant.menu.plats.map((m) => (
+                        <MenuItem  item={m}/>
+                    ))}
+                </div>
+                <div className={styles.menuContainer}>
+                    <h3>DESSERTS</h3>
+                    {restaurant.menu.desserts.map((m) => (
+                        <MenuItem  item={m}/>
+                    ))}
+                </div>
+                <button className={styles.cta}>Commander</button>
+            </div>
+        </>
     )
 }
